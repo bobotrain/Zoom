ba5bd394dda25ad241b50b975707771555be4884
최경보 <chlrudqh97@naver.com>
2023-08-31 23:59:10 +0900

    0831

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,46 +1,21 @@
-const messageList = document.querySelector("ul");
-const nickForm = document.querySelector("#nick");
-const messageForm = document.querySelector("#message");
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = io();
 
-//JSON을 문자열로 변환하기
-function makeMessage(type, payload){
-    const msg = { type, payload};
-    return JSON.stringify(msg);
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+const room = document.getElementById("room");
+
+room.hidden = true;
+
+function showRoom(){
+    welcome.hidden = true;
+    room.hidden = false;
 }
 
-
-//연결되었을 때
-socket.addEventListener("open", () => {
-    console.log("Connected to Server");
-})
-
-//서버로부터 메시지를 받았을 때 때
-socket.addEventListener("message", (message) => {
-    const li = document.createElement("li");
-    li.innerText = message.data;
-    messageList.append(li);
-})
-
-//서버가 오프라인 되었을 때
-socket.addEventListener("close", () => {
-    console.log("Disconnected from Server");
-})
-//메시지 이벤트 처리
-function handleSubmit(event){
+function handleRoomSubmit(event){
     event.preventDefault();
-    const input = messageForm.querySelector("input");
-    socket.send(makeMessage("new_message", input.value));
-    input.value = "";
-}
-//닉네임 이벤트처리
-function handleNickSubmit(event){
-    event.preventDefault();
-    const input = nickForm.querySelector("input");
-    socket.send(makeMessage("nickname", input.value));
+    const input = form.querySelector("input");
+    socket.emit("enter_room", input.value, showRoom); 
     input.value = "";
 }
 
-//서버로 메시지 보내기
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+form.addEventListener("submit", handleRoomSubmit);
